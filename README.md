f9c5fd4c71e1a379bbfd900aefcdc00f7d9c2879
Jonathan Ludvigsen <124934498+JJ-Than@users.noreply.github.com>
2024-12-16 09:21:45 -0500

    Created DB creation logic

--- README.md
+++ README.md
@@ -4,19 +4,21 @@
 ## POST Request JSON Format
 Within the POST request, there will be multiple fields which will be tracked. Please see below for a list of fields which must be present.
 
-ID (int), time (string), type (string: 'key', 'line', or 'file'), submission (string)
+ID (int), time (string), status (int... 0: Pass, 1: Fail, 2: (NMP) Grading Offload), submission (list)
 
 ## Project Feature List
 Below are some anticipated features. If a feature is not checked off, then it is not completed yet.
 
 ### Configuration of HTTP Server
 - [ ] Port Number
-- [ ] 
+- [ ] IP Address
+- [ ] Computer
 
 ### Configuration of File Grading Procedures
-- [ ] Key/Value Parsing: File content changes (useful for checking if configuration files contain the correct configuration)
-- [ ] Line Parsing: Search for custom line attributes within files (useful for checking logfiles to ensure the correct items are being logged)
-- [ ] Hash Verification: Comparison of key file to specified file(s) to validate if an input is correct (useful to check if two files have matching content)
+- [ ] Key/Value: File content changes (useful for checking if configuration files contain the correct configuration)
+- [ ] Line: Search for custom line attributes within files (useful for checking logfiles to ensure the correct items are being logged)
+- [ ] Hash: Comparison of key file to specified file(s) to validate if an input is correct (useful to check if two files have matching content)
+- [ ] Default Grade: (auto-set to 0.0)
 
 ### Grade Reporting Methods
 - [ ] CSV Files
--- sqlite_setup.py
+++ sqlite_setup.py
@@ -2,12 +2,18 @@ import sqlite3
 import os
 from configparser import ConfigParser
 
-def default_config():
+def default_config(create_db: bool, demo_mode: bool):
     config = ConfigParser()
-    config['general'] = {'db_type': 'SQLite', 'db_filepath': './autograder.db'}
+    db_filepath = './autograder.db'
+    config['general'] = {'db_type': 'SQLite', 'db_filepath': db_filepath, 'demo_mode': demo_mode}
+    config['grading'] = {'default_weight': 1}
+    config['lab'] = {'default_length': 60}
 
     with open('config.ini', 'w') as file:
         config.write(file)
+
+    if create_db == True:
+        create_db(db_filepath, demo_mode)
 
 def config_reader(keys: list[str]) -> dict:
     config = ConfigParser()
@@ -15,16 +21,70 @@ def config_reader(keys: list[str]) -> dict:
 
     config.default_section = 'general'
 
+    
+
 def create_db(path: str, create_samples: bool):
+    config = config_reader(['grading/default_weight', 'lab/default_length']) # TODO Allow different section parsing of config file with forward slash '/'
     with sqlite3.connect(path) as conn:
         cursor = conn.cursor()
 
         cursor.execute('''
+            CREATE TABLE IF NOT EXIST LabList (
+                        LabID INTEGER PRIMARY KEY,
+                        LabName TEXT NOT NULL,
+                        CreationTime DATETIME,
+                        VerifyAnswerBool INTEGER NOT NULL CHECK (VerifyAnswer IN (0, 1)),
+                        OriginalLab INTEGER,
+                        FOREIGN KEY (OriginalLab) REFERENCES LabList(LabID)
+                       );
+                       
+            CREATE TABLE IF NOT EXIST AnswerKey (
+                        LabID INTEGER NOT NULL,
+                        AnswerID INTEGER NOT NULL,
+                        AnswerType TEXT NOT NULL CHECK (LabType IN ('Key', 'Line', 'Hash')),
+                        AnswerWeight INTEGER NOT NULL DEFAULT {default_weight},
+                        Answer TEXT NOT NULL,
+                        PRIMARY KEY (LabID, AnswerID),
+                        FOREIGN KEY (LabID) REFERENCES LabList(LabID)
+                       );
+                       
+            CREATE TABLE IF NOT EXIST ActiveLabs (
+                        RequestID INTEGER PRIMARY KEY,
+                        LabID INTEGER NOT NULL,
+                        DeploymentTime DATETIME NOT NULL,
+                        TimeLimit INTEGER NOT NULL DEFAULT {default_length},
+                        FOREIGN KEY (LabID) REFERENCES LabList(LabID)
+                       );
+                       
+            CREATE TABLE IF NOT EXIST CompleteLabs (
+                        RequestID INTEGER PRIMARY KEY,
+                        LabID INTEGER NOT NULL,
+                        SubmissionID INTEGER NOT NULL,
+                        DeploymentTime DATETIME NOT NULL,
+                        TimeLimit INTEGER NOT NULL,
+                        CompletionTime DATETIME NOT NULL,
+                        FOREIGN KEY (LabID) REFERENCES LabList(LabID)
+                       );
+                       
+            CREATE TABLE IF NOT EXIST SubmittedAnswers (
+                        RequestID INTEGER NOT NULL,
+                        SubmittedAnswer INTEGER NOT NULL,
+                        SubmissionID INTEGER NOT NULL,
+                        Submission TEXT NOT NULL,
+                        PRIMARY KEY (RequestID, SubmittedAnswer),
+                        FOREIGN KEY (RequestID) REFERENCES CompletedLabs(RequestID)
+                       ); '''
+        ).format(default_weight=config['default_weight'], default_config=config['default_config'])
 
-''')
+        conn.commit()
 
 if __name__ == '__main__':
     file = './config.ini'
     if os.path.exists(file) and os.path.isfile(file):
         config = config_reader(['db_path'])
-        if os.path.exists(config['db_path']):
+        if not os.path.exists(config['db_path']):
+            create_db(path=config['db_path'], create_samples=True)
+        else:
+            print('Database already exists at {db_path}. If you wish to reset it, please delete this file and run the script again.'.format(config['db_path']))
+    else:
+        default_config(create_db=True, demo_mode=True)
